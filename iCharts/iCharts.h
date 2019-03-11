4e0c57712e3d6161def0fecd34f167199f20c096
Volodymyr Hryhoriev <volodja.grigoriev@gmail.com>
2019-03-11 14:07:22 +0200

    Initial Commit

---
+++ iCharts/iCharts.h
@@ -0,0 +1,19 @@
+//
+//  iCharts.h
+//  iCharts
+//
+//  Created by Volodymyr Hryhoriev on 3/11/19.
+//  Copyright © 2019 Volodymyr Hryhoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iCharts.
+FOUNDATION_EXPORT double iChartsVersionNumber;
+
+//! Project version string for iCharts.
+FOUNDATION_EXPORT const unsigned char iChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iCharts/PublicHeader.h>
+
+
